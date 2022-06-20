7b84ca71b850c1aff93bfae31e7b94fe7904cd8e
Lemus <lemuslau@amazon.com>
2022-06-19 19:37:32 -0700

    Used for Deliverable 2

--- scraping.py
+++ scraping.py
@@ -14,14 +14,15 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    
     # Run all scraping functions and store results in dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere_image_urls": hemisphere_image_urls(browser)
     }
     # Stop webdriver and return data
     browser.quit()
@@ -45,7 +46,7 @@ def mars_news(browser):
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
     
-      except AttributeError:
+    except AttributeError:
         return None, None
 
     return news_title, news_p
